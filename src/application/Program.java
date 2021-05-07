414827d6fd7cdc8d6f76f2ef402071d2a22641cb
Ralph Aguiar <ralphaguiar@gmail.com>
2021-05-07 17:21:21 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "LIVROS");
+		
+		Seller sell = new Seller(21, "Buceto", "buceto@gmail.com", new Date(), 4000.0, obj);
+		
 		System.out.println(obj);
+		System.out.println(sell);
 	}
 
 }
