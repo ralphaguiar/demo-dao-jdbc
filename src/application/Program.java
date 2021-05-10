bead2c2a8023f21265f3459bca10129b65b245f3
Ralph Aguiar <ralphaguiar@gmail.com>
2021-05-10 11:17:34 -0300

    findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -24,6 +24,13 @@ public class Program {
 		
 		//System.out.println(obj);
 		System.out.println(sell);
+		
+		System.out.println("\n==== TESTE 2 Seller findByDepartment ====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
